359e44896f68ee2d930d74cabeab619807b1f5c9
SwalmMan <52794465+SwalmMan@users.noreply.github.com>
2019-07-12 18:16:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`abc |By Slim Man#9063`,"http://twitch.tv/Death Shop")
+client.user.setGame(`abc |.Dirillis`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
